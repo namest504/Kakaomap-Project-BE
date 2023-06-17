8a73f18205e3f1f98cbf25c1e2ebadc5b4db1284
List <61047602+namest504@users.noreply.github.com>
2023-06-18 02:27:38 +0900

    test: 가게 등록 테스트

--- src/test/java/com/list/kakaoMap/controller/StoreControllerTest.java
+++ src/test/java/com/list/kakaoMap/controller/StoreControllerTest.java
@@ -1,56 +1,64 @@
 package com.list.kakaoMap.controller;
 
-import com.list.kakaoMap.entity.Store;
-import com.list.kakaoMap.service.StoreService;
-import jakarta.transaction.Transactional;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static com.list.kakaoMap.dto.StoreDto.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
-@Transactional
+@AutoConfigureMockMvc
+//@WebMvcTest(StoreController.class)
 class StoreControllerTest {
 
     @Autowired
-    StoreService storeService;
+    MockMvc mockMvc;
 
-    @BeforeEach
-    void before() {
-        Store store = Store.builder()
-                .id(1L)
-                .name("test_7e6b067e0783")
-                .detail("test_c5d923cc5984")
-                .posX(97.45)
-                .posY(72.58)
-                .build();
+    @Autowired
+    ObjectMapper objectMapper;
 
-        StoreRequest add = new StoreRequest(store.getName(), store.getDetail(), store.getPosX(), store.getPosY());
-        storeService.addStore(add);
+    private <T> String toJson(T data) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(data);
     }
 
     @Test
     @DisplayName("가게 등록 테스트")
-    public void addTest() {
-        List<Store> result = storeService.findStore("test_7e6b067e0783");
+    public void addTest() throws Exception {
+        //given
+        StoreRequest storeRequest = new StoreRequest("test_7e6b067e0783", "test_c5d923cc5984", 97.45, 72.58);
 
-        Assertions.assertThat(result.get(0).getName()).isEqualTo("test_7e6b067e0783");
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post("/store/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(storeRequest)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.name").value("test_7e6b067e0783"));
+    }
+
+    @Test
+    @DisplayName("가게 조회 테스트")
+    public void findTest() throws Exception {
+        //given
+        //when
+        //then
     }
 
     @Test
     @DisplayName("가게 정보 수정 테스트")
-    public void editTest() {
-        Store store = storeService.findStore("test_7e6b067e0783").get(0);
-        StoreRequest editAdd = new StoreRequest("editedtest_7e6b067e0783", store.getDetail(), store.getPosX(), store.getPosY());
-
-        Store editedStore = storeService.editStore(1L, editAdd);
-
-        Assertions.assertThat(editedStore.getName()).isEqualTo("editedtest_7e6b067e0783");
+    public void editTest() throws Exception {
+        //given
+        //when
+        //then
     }
 }
