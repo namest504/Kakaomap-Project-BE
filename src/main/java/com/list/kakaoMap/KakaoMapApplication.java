6a204f9121b3f4a129a51be38b58f613467c1c4d
List <61047602+namest504@users.noreply.github.com>
2023-06-17 23:20:55 +0900

    init

---
+++ src/main/java/com/list/kakaoMap/KakaoMapApplication.java
@@ -0,0 +1,13 @@
+package com.list.kakaoMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/list/kakaoMap/KakaoMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.list.kakaoMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
