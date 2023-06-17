b010b6bee93b42df8469f00d74adf3168e9c2c86
List <61047602+namest504@users.noreply.github.com>
2023-06-17 23:33:41 +0900

    feat: Repository 생성

---
+++ src/main/java/com/list/kakaoMap/repository/StoreRepository.java
@@ -0,0 +1,9 @@
+package com.list.kakaoMap.repository;
+
+import com.list.kakaoMap.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
