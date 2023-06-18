97bd4bb8315fe3f7a8d1dea5733047c58053133c
List <61047602+namest504@users.noreply.github.com>
2023-06-18 17:25:27 +0900

    test: 가게 삭제 Controller

--- src/test/java/com/list/kakaoMap/controller/StoreControllerTest.java
+++ src/test/java/com/list/kakaoMap/controller/StoreControllerTest.java
@@ -1,23 +1,28 @@
 package com.list.kakaoMap.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.list.kakaoMap.entity.Store;
-import com.list.kakaoMap.repository.StoreRepository;
+import com.list.kakaoMap.service.StoreService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.list.kakaoMap.dto.StoreDto.*;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -31,12 +36,8 @@ class StoreControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
-    @Autowired
-    StoreRepository storeRepository;
-
-    private <T> String toJson(T data) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(data);
-    }
+    @Mock
+    StoreService storeService;
 
     @Test
     @DisplayName("가게 등록 테스트")
@@ -44,13 +45,18 @@ class StoreControllerTest {
     public void addTest() throws Exception {
         //given
         StoreRequest storeRequest = new StoreRequest("test_7e6b067e0783", "test_c5d923cc5984", 97.45, 72.58);
+        Store storeResponse = new Store(1L, "test_7e6b067e0783", "test_c5d923cc5984", 97.45, 72.58);
+        given(storeService.addStore(storeRequest))
+                .willReturn(storeResponse);
+        String json = new ObjectMapper().writeValueAsString(storeRequest);
 
         //when
+        ResultActions perform = mockMvc.perform(MockMvcRequestBuilders.post("/store")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json));
+
         //then
-        mockMvc.perform(MockMvcRequestBuilders.post("/store/add")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(toJson(storeRequest)))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+        perform.andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.name").value("test_7e6b067e0783"))
                 .andDo(print());
@@ -61,25 +67,30 @@ class StoreControllerTest {
     @Transactional
     public void findTest() throws Exception {
         //given
-        Store store = Store.builder()
-                .name("test_7e6b067e0783")
-                .detail("test_c5d923cc5984")
-                .posX(97.45)
-                .posY(72.58)
-                .build();
+        Store storeResponse1 = new Store(1L, "test_7e6b067e0783", "test_c5d923cc5984", 97.45, 72.58);
+        Store storeResponse2 = new Store(2L, "test_1123klnsas", "test_29sne215231", 23.56, 28.65);
+        Store storeResponse3 = new Store(3L, "test_24901hsksa", "test_32kjba9e1", 55.27, 81.21);
+        List<Store> storeResponses = new ArrayList<>();
+        storeResponses.add(storeResponse1);
+        storeResponses.add(storeResponse2);
+        storeResponses.add(storeResponse3);
+
+        String json = new ObjectMapper().writeValueAsString(storeResponses);
+
+        given(storeService.findStore("test"))
+                .willReturn(storeResponses);
 
         MultiValueMap<String, String> param = new LinkedMultiValueMap<>();
         param.add("name", "test");
-        //when
-        storeRepository.save(store);
 
+        //when
+        ResultActions perform = mockMvc.perform(MockMvcRequestBuilders.get("/store")
+                .param("name","test"));
+//                .contentType(MediaType.APPLICATION_JSON));
         //then
-        mockMvc.perform(MockMvcRequestBuilders.get("/store/find")
-                        .params(param)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+        perform.andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(jsonPath("$.success").value(true))
-                .andExpect(jsonPath("$.result").exists())
+//                .andExpect(jsonPath("$.result").exists())
                 .andDo(print());
     }
 
@@ -88,24 +99,39 @@ class StoreControllerTest {
     @Transactional
     public void editTest() throws Exception {
         //given
+        StoreRequest storeRequest = new StoreRequest("test_12523as", "test_29sne215231", 23.56, 28.65);
         Store store = Store.builder()
-                .name("test_7e6b067e0783")
-                .detail("test_c5d923cc5984")
-                .posX(97.45)
-                .posY(72.58)
+                .id(2L)
+                .name("test_12523as")
+                .detail("test_29sne215231")
+                .posX(23.56)
+                .posY(28.65)
                 .build();
-
-        StoreRequest storeRequest = new StoreRequest("test_7e6b067e0783", "test_c5d923cc5984", 97.45, 72.58);
+        String json = new ObjectMapper().writeValueAsString(storeRequest);
+        given(storeService.editStore(2L, storeRequest))
+                .willReturn(store);
         //when
-        Store save = storeRepository.save(store);
-
+        ResultActions perform = mockMvc.perform(MockMvcRequestBuilders.put("/store/" + 2)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json));
         //then
-        mockMvc.perform(MockMvcRequestBuilders.put("/store/edit/" + save.getId())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(toJson(storeRequest)))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+        perform.andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(jsonPath("$.success").value(true))
-                .andExpect(jsonPath("$.name").value("test_7e6b067e0783"))
+                .andExpect(jsonPath("$.name").value("test_12523as"))
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("가게 삭제 테스트")
+    @Transactional
+    public void deleteTest() throws Exception {
+        //given
+        given(storeService.deleteStore(1L))
+                .willReturn(true);
+        //when
+        ResultActions perform = mockMvc.perform(MockMvcRequestBuilders.delete("/store/" + 1));
+        //then
+        perform.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.success").value(true));
     }
 }
