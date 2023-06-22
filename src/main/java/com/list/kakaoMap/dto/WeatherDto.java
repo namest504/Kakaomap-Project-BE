82422f4381bfa6259503992666810b50563fb928
List <61047602+namest504@users.noreply.github.com>
2023-06-22 22:33:29 +0900

    feat: 오픈 API DTO 추가

---
+++ src/main/java/com/list/kakaoMap/dto/WeatherDto.java
@@ -0,0 +1,73 @@
+package com.list.kakaoMap.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+public class WeatherDto {
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class WeatherResponse {
+        private String dataType;
+        private List<Items> item;
+    }
+
+    @Getter
+    @Setter
+    public static class WeatherApiResponse {
+        private Response response;
+    }
+
+    @Getter
+    @Setter
+    public static class Response {
+        private Header header;
+        private Body body;
+    }
+
+    @Getter
+    @Setter
+    public static class Header {
+        private String resultCode;
+        private String resultMsg;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Body {
+        private String dataType;
+        private Items items;
+        private int pageNo;
+        private int numOfRows;
+        private int totalCount;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Items {
+        private List<Item> item;
+    }
+
+    @Getter
+    @Setter
+    public static class Item {
+        private String baseDate;
+        private String baseTime;
+        private String category;
+        private String fcstDate;
+        private String fcstTime;
+        private String fcstValue;
+        private int nx;
+        private int ny;
+    }
+}
